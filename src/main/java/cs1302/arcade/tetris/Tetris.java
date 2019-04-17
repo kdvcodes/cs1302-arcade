9aeec12734af96662ad4955ffa49344e0361b241
Kevin Vo <kdvcodes@gmail.com>
2019-04-17 12:41:49 -0400

    added stage and scene elements to show stage for the game.
    No specific specifications has been made yet.

--- src/main/java/cs1302/arcade/tetris/Tetris.java
+++ src/main/java/cs1302/arcade/tetris/Tetris.java
@@ -2,21 +2,39 @@ package cs1302.arcade.tetris;
 
 import java.util.Random;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * This is the Tetris game class
  *
  */
-public class Tetris implements Runnable {
+public class Tetris extends Stage implements Runnable {
 	
 	Board game;
 	Tetrimino currentPiece;
 	Random generator;
 	int level;
-
+	
+	public Tetris() {
+		super();
+		VBox vbox = new VBox();
+		HBox tetrisMainFrame = new HBox(vbox);
+		tetrisMainFrame.getChildren().add(vbox);
+		
+		Scene tetrisScene = new Scene(tetrisMainFrame);
+		this.setScene(tetrisScene);
+		this.setHeight(900);
+		this.setWidth(450);
+		this.show();
+	} // Tetris constructor
+	
 	@Override
 	public void run() {
 		game = new Board();
 		generator = new Random();
-	}
+	} // run
 
 }
