fc034c12342e5d9df8e8ca9916200f4e9de6e7df
Ryan McCullough <rwm30074@uga.edu>
2019-04-18 00:48:36 -0400

    Began to implement update cycle

--- src/main/java/cs1302/arcade/tetris/Tetris.java
+++ src/main/java/cs1302/arcade/tetris/Tetris.java
@@ -2,16 +2,21 @@ package cs1302.arcade.tetris;
 
 import java.util.Random;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * This is the Tetris game class
  *
  */
-public class Tetris extends Stage implements Runnable {
+public class Tetris extends Stage {
 	
 	Board game;
 	Tetrimino currentPiece;
@@ -20,6 +25,8 @@ public class Tetris extends Stage implements Runnable {
 	
 	private final int rows = 22;
 	private final int columns = 10;
+	
+	private final int framerate = 60;
 	
 	public Tetris(int level) {
 		game = new Board(rows, columns);
@@ -37,10 +44,10 @@ public class Tetris extends Stage implements Runnable {
 		this.setHeight(600);
 		this.setWidth(450);
 		this.show();
+		Timeline t = new Timeline(new KeyFrame(Duration.millis(1000 / framerate), this::update));
 	} // Tetris constructor
 
-	@Override
-	public void run() {
+	public void update(ActionEvent r) {
 		for (int i = 0; i < rows; i++) {
 			if (game.lineFull(i)) {
 				game.clearLine(i);
